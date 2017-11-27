3c7434ac21b0086dfb6ad1862845a44276d429ae
Yudhvir Raj <rajyudhvir29@gmail.com>
2017-11-26 21:51:15 -0800

    Finished chat view

--- joeDemo/Supporting Files/Constants.swift
+++ joeDemo/Supporting Files/Constants.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2017 User. All rights reserved.
 //
 
-import Foundation
+import Firebase
+
+struct Constants
+{
+    struct refs
+    {
+        static let databaseRoot = Database.database().reference()
+        static let databaseChats = databaseRoot.child("chats")
+    }
+}
